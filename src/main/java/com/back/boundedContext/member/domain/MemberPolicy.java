734a07312ef7099b65941d513f851d4ea26e6bff
NamSangwon <nsw3265@naver.com>
2025-12-24 10:23:43 +0900

    0010

---
+++ src/main/java/com/back/boundedContext/member/domain/MemberPolicy.java
@@ -0,0 +1,25 @@
+package com.back.boundedContext.member.domain;
+
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Service
+public class MemberPolicy {
+    private static final int PASSWORD_CHANGE_INTERVAL_DAYS = 90;
+
+    public Duration getNeedToCHangePassowrdPeriod() {
+        return Duration.ofDays(PASSWORD_CHANGE_INTERVAL_DAYS);
+    }
+
+    public int getNeedToChangePasswordDays() {
+        return PASSWORD_CHANGE_INTERVAL_DAYS;
+    }
+
+    public boolean isNeedToChangePassword(LocalDateTime lastChangedAt) {
+        if (lastChangedAt == null) return true;
+
+        return lastChangedAt.plusDays(PASSWORD_CHANGE_INTERVAL_DAYS).isBefore(LocalDateTime.now());
+    }
+}
