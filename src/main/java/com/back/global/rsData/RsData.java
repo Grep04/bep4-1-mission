3dd62ab7d3d1224696777496af8a1441d5a70efa
NamSangwon <nsw3265@naver.com>
2025-12-24 11:30:57 +0900

    0013

--- src/main/java/com/back/global/rsData/RsData.java
+++ src/main/java/com/back/global/rsData/RsData.java
@@ -2,7 +2,11 @@ package com.back.global.rsData;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.ResponseEntity;
 
+/**
+ * 응답 상태 규격화 코드 (= ResponseEntity 객체를 대체)
+ */
 @AllArgsConstructor
 @Getter
 public class RsData<T> {
