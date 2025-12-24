3a873b16d6a6cf0a9d835b57bd304ba83fa2c439
NamSangwon <nsw3265@naver.com>
2025-12-24 11:51:57 +0900

    0014

--- src/main/java/com/back/global/initData/DataInit.java
+++ src/main/java/com/back/global/initData/DataInit.java
@@ -4,6 +4,7 @@ import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.member.app.MemberFacade;
 import com.back.boundedContext.post.app.PostFacade;
+import com.back.global.rsData.RsData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -15,13 +16,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class DataInit {
     private final DataInit self;
-    private final MemberFacade memberService;
-    private final PostFacade postService;
+    private final MemberFacade memberFacade;
+    private final PostFacade postFacade;
 
-    public DataInit(@Lazy DataInit self, MemberFacade memberService, PostFacade postService) {
+    public DataInit(@Lazy DataInit self, MemberFacade memberFacade, PostFacade postFacade) {
         this.self = self;
-        this.memberService = memberService;
-        this.postService = postService;
+        this.memberFacade = memberFacade;
+        this.postFacade = postFacade;
     }
 
     @Bean
@@ -35,43 +36,54 @@ public class DataInit {
 
     @Transactional
     public void makeBaseMembers() {
-        if (memberService.count() > 0) return;
+        if (memberFacade.count() > 0) return;
 
-        Member systemMember = memberService.join("system", "1234", "시스템").getData();
-        Member holdingMember = memberService.join("holding", "1234", "홀딩").getData();
-        Member adminMember = memberService.join("admin", "1234", "관리자").getData();
-        Member user1Member = memberService.join("user1", "1234", "유저1").getData();
-        Member user2Member = memberService.join("user2", "1234", "유저2").getData();
-        Member user3Member = memberService.join("user3", "1234", "유저3").getData();
+        Member systemMember = memberFacade.join("system", "1234", "시스템").getData();
+        Member holdingMember = memberFacade.join("holding", "1234", "홀딩").getData();
+        Member adminMember = memberFacade.join("admin", "1234", "관리자").getData();
+        Member user1Member = memberFacade.join("user1", "1234", "유저1").getData();
+        Member user2Member = memberFacade.join("user2", "1234", "유저2").getData();
+        Member user3Member = memberFacade.join("user3", "1234", "유저3").getData();
     }
 
     @Transactional
     public void makeBasePosts() {
-        if (postService.count() > 0) return;
-        Member user1Member = memberService.findByUsername("user1").get();
-        Member user2Member = memberService.findByUsername("user2").get();
-        Member user3Member = memberService.findByUsername("user3").get();
+        if (postFacade.count() > 0) return;
+        Member user1Member = memberFacade.findByUsername("user1").get();
+        Member user2Member = memberFacade.findByUsername("user2").get();
+        Member user3Member = memberFacade.findByUsername("user3").get();
 
-        Post post1 = postService.write(user1Member, "제목1", "내용1").getData();
-        Post post2 = postService.write(user1Member, "제목2", "내용2").getData();
-        Post post3 = postService.write(user1Member, "제목3", "내용3").getData();
-        Post post4 = postService.write(user2Member, "제목4", "내용4").getData();
-        Post post5 = postService.write(user2Member, "제목5", "내용5").getData();
-        Post post6 = postService.write(user3Member, "제목6", "내용6").getData();
+        RsData<Post> post1RsData = postFacade.write(user1Member, "제목1", "내용1");
+        log.debug(post1RsData.getMsg());
+
+        RsData<Post> post2RsData = postFacade.write(user1Member, "제목2", "내용2");
+        log.debug(post2RsData.getMsg());
+
+        RsData<Post> post3RsData = postFacade.write(user1Member, "제목3", "내용3");
+        log.debug(post3RsData.getMsg());
+
+        RsData<Post> post4RsData = postFacade.write(user2Member, "제목4", "내용4");
+        log.debug(post4RsData.getMsg());
+
+        RsData<Post> post5RsData = postFacade.write(user2Member, "제목5", "내용5");
+        log.debug(post5RsData.getMsg());
+
+        RsData<Post> post6RsData = postFacade.write(user3Member, "제목6", "내용6");
+        log.debug(post6RsData.getMsg());
     }
 
     @Transactional
     public void makeBasePostComments() {
-        Post post1 = postService.findById(1).get();
-        Post post2 = postService.findById(2).get();
-        Post post3 = postService.findById(3).get();
-        Post post4 = postService.findById(4).get();
-        Post post5 = postService.findById(5).get();
-        Post post6 = postService.findById(6).get();
+        Post post1 = postFacade.findById(1).get();
+        Post post2 = postFacade.findById(2).get();
+        Post post3 = postFacade.findById(3).get();
+        Post post4 = postFacade.findById(4).get();
+        Post post5 = postFacade.findById(5).get();
+        Post post6 = postFacade.findById(6).get();
 
-        Member user1Member = memberService.findByUsername("user1").get();
-        Member user2Member = memberService.findByUsername("user2").get();
-        Member user3Member = memberService.findByUsername("user3").get();
+        Member user1Member = memberFacade.findByUsername("user1").get();
+        Member user2Member = memberFacade.findByUsername("user2").get();
+        Member user3Member = memberFacade.findByUsername("user3").get();
 
         if (post1.hasComments()) return;
 
