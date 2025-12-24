5c9bee456db6aec69d604f3d13c34c2377adbf29
NamSangwon <nsw3265@naver.com>
2025-12-24 10:33:13 +0900

    0011

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -4,10 +4,10 @@ import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.EventPublsiher.EventPublisher;
+import com.back.global.rsData.RsData;
 import com.back.shared.post.dto.PostDto;
 import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +16,7 @@ public class PostWriteUseCase {
     private final PostRepository postRepository;
     private final EventPublisher eventPublisher;
 
-    public Post write(Member author, String title, String content) {
+    public RsData<Post> write(Member author, String title, String content) {
         Post post = postRepository.save(new Post(author, title, content));
 
         // 결합도 분리를 위한 이벤트 발행
@@ -24,6 +24,6 @@ public class PostWriteUseCase {
                 new PostCreatedEvent(new PostDto(post))
         );
 
-        return post;
+        return new RsData<>("201-1", "%d번 글이 생성되었습니다.".formatted(post.getId()), post);
     }
 }
