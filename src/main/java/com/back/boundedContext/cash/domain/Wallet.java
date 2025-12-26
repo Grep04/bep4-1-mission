2c0f99fcdaace2ea9cc8a1991b289b35f63a31e4
NamSangwon <nsw3265@naver.com>
2025-12-26 13:57:06 +0900

    0025

--- src/main/java/com/back/boundedContext/cash/domain/Wallet.java
+++ src/main/java/com/back/boundedContext/cash/domain/Wallet.java
@@ -1,13 +1,19 @@
 package com.back.boundedContext.cash.domain;
 
+import com.back.global.jpa.entity.BaseEntity;
 import com.back.global.jpa.entity.BaseManualIdAndTime;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    동시 다발적인 결제 요청 상황에서
+    재고 데이터의 무결성을 보장하기 위해
+    JPA의 비관적 락(Pessimistic Lock)을 활용
+ */
 @Entity
 @Table(name = "CASH_WALLET")
 @Getter
@@ -16,8 +22,65 @@ public class Wallet extends BaseManualIdAndTime {
     @ManyToOne(fetch = FetchType.LAZY)
     private CashMember holder;
 
+    // 특정 필드에 @Getter를 명시함으로써,
+    // 이 필드는 외부에서 빈번하게 조회되는 핵심 속성임을 문서화
+    @Getter
+    private long balance;
+
+    // 특정 Wallet 객체와 연관된 CashLog 엔티티들만 리스트로 관리
+    @OneToMany(mappedBy = "wallet", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
+    private List<CashLog> cashLogs = new ArrayList<>();
+
     public Wallet(CashMember holder) {
-        super(holder.getId());
+        super(holder.getId()); // 지갑 ID = 회원 ID
         this.holder = holder;
+    }
+
+    public boolean hasBalance() {
+        return balance > 0;
+    }
+
+    public void credit(long amount, CashLog.EventType eventType, String relTypeCode, int relId) {
+        balance += amount;
+
+        addCashLog(amount, eventType, relTypeCode, relId);
+    }
+
+    public void credit(long amount, CashLog.EventType eventType, BaseEntity rel) {
+        credit(amount, eventType, rel.getModelTypeCode(), rel.getId());
+    }
+
+    public void credit(long amount, CashLog.EventType eventType) {
+        credit(amount, eventType, holder);
+    }
+
+    public void debit(long amount, CashLog.EventType eventType, String relTypeCode, int relId) {
+        balance -= amount;
+
+        addCashLog(-amount, eventType, relTypeCode, relId);
+    }
+
+    public void debit(long amount, CashLog.EventType eventType, BaseEntity rel) {
+        debit(amount, eventType, rel.getModelTypeCode(), rel.getId());
+    }
+
+    public void debit(long amount, CashLog.EventType eventType) {
+        debit(amount, eventType, holder);
+    }
+
+    private CashLog addCashLog(long amount, CashLog.EventType eventType, String relTypeCode, int relId) {
+        CashLog cashLog = new CashLog(
+                eventType,
+                relTypeCode,
+                relId,
+                holder,
+                this,
+                amount,
+                balance
+        );
+
+        cashLogs.add(cashLog);
+
+        return cashLog;
     }
 }
