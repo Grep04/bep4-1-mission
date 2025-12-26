5d445e4b78b2075e0d2b2cbeeda2f27d30667e9e
NamSangwon <nsw3265@naver.com>
2025-12-26 15:24:41 +0900

    0027

--- src/main/java/com/back/boundedContext/cash/app/CashCreateWalletUseCase.java
+++ src/main/java/com/back/boundedContext/cash/app/CashCreateWalletUseCase.java
@@ -2,7 +2,9 @@ package com.back.boundedContext.cash.app;
 
 import com.back.boundedContext.cash.domain.CashMember;
 import com.back.boundedContext.cash.domain.Wallet;
+import com.back.boundedContext.cash.out.CashMemberRepository;
 import com.back.boundedContext.cash.out.WalletRepository;
+import com.back.shared.cash.dto.CashMemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +12,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CashCreateWalletUseCase {
     private final WalletRepository walletRepository;
+    private final CashMemberRepository cashMemberRepository;
 
-    public Wallet createCashMember(CashMember member) {
-        return walletRepository.save(new Wallet(member));
+    public Wallet createCashMember(CashMemberDto member) {
+        // getReferenceById : 영속성 컨텍스트에 있는 엔티티를 참조
+        // 즉, Select 쿼리가 실행되지 않음
+        CashMember _member = cashMemberRepository.getReferenceById(member.getId());
+
+        return walletRepository.save(new Wallet(_member));
     }
 }
--- src/main/java/com/back/boundedContext/cash/app/CashSyncMemberUseCase.java
+++ src/main/java/com/back/boundedContext/cash/app/CashSyncMemberUseCase.java
@@ -2,6 +2,9 @@ package com.back.boundedContext.cash.app;
 
 import com.back.boundedContext.cash.domain.CashMember;
 import com.back.boundedContext.cash.out.CashMemberRepository;
+import com.back.global.EventPublsiher.EventPublisher;
+import com.back.shared.cash.dto.CashMemberDto;
+import com.back.shared.cash.event.CashMemberCreatedEvent;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,18 +13,38 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CashSyncMemberUseCase {
     private final CashMemberRepository cashMemberRepository;
+    private final EventPublisher eventPublisher;
 
     public CashMember syncMember(MemberDto member) {
-        CashMember cashMember = new CashMember(
-                member.getId(),
-                member.getCreateDate(),
-                member.getModifyDate(),
-                member.getUsername(),
-                "",
-                member.getNickname(),
-                member.getActivityScore()
+        boolean isNew = !cashMemberRepository.existsById(member.getId());
+
+        CashMember _member = cashMemberRepository.save(
+                new CashMember(
+                        member.getId(),
+                        member.getCreateDate(),
+                        member.getModifyDate(),
+                        member.getUsername(),
+                        "",
+                        member.getNickname(),
+                        member.getActivityScore()
+                )
         );
 
-        return cashMemberRepository.save(cashMember);
+        if (isNew) {
+            // CashMemberCreatedEvent 를 발행하여 Wallet 생성 처리
+            // 강력한 모듈화와 시스템 확장성을 위한 설계 (EDD)
+            /*
+                1. 단일 책임 원칙 (Single Responsibility Principle)
+                2. 서비스 간 및 모듈 간 느슨한 결합 (Loose Coupling)
+                3. 장애 격리 및 복구 (Fault Tolerance)
+            */
+            eventPublisher.publish(
+                    new CashMemberCreatedEvent(
+                            new CashMemberDto(_member)
+                    )
+            );
+        }
+
+        return _member;
     }
 }
