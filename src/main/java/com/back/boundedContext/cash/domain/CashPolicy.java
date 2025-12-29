561d93a989962e2aaf292e02bec2fa848ead2043
NamSangwon <nsw3265@naver.com>
2025-12-29 14:49:10 +0900

    0035

---
+++ src/main/java/com/back/boundedContext/cash/domain/CashPolicy.java
@@ -0,0 +1,5 @@
+package com.back.boundedContext.cash.domain;
+
+public class CashPolicy {
+    public static final int HOLDING_MEMBER_ID = 2; // 2번 회원 = 운영팀 (홀딩용 아이디)
+}
--- src/main/java/com/back/boundedContext/cash/in/CashEventListner.java
+++ src/main/java/com/back/boundedContext/cash/in/CashEventListner.java
@@ -3,6 +3,7 @@ package com.back.boundedContext.cash.in;
 import com.back.boundedContext.cash.app.CashFacade;
 import com.back.boundedContext.cash.domain.CashMember;
 import com.back.shared.cash.event.CashMemberCreatedEvent;
+import com.back.shared.market.event.MarketOrderPaymentRequestedEvent;
 import com.back.shared.member.event.MemberJoinedEvent;
 import com.back.shared.member.event.MemberModifiedEvent;
 import lombok.RequiredArgsConstructor;
@@ -12,26 +13,35 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
+import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMIT;
+
 @Component
 @RequiredArgsConstructor
 public class CashEventListner {
     private final CashFacade cashFacade;
 
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = REQUIRES_NEW)
     public void handle(MemberJoinedEvent event) {
         cashFacade.syncMember(event.getMember());
     }
 
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = REQUIRES_NEW)
     public void handle(MemberModifiedEvent event) {
         cashFacade.syncMember(event.getMember());
     }
 
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = REQUIRES_NEW)
     public void handle(CashMemberCreatedEvent event) {
         cashFacade.createWallet(event.getMember());
+    }
+
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = REQUIRES_NEW)
+    public void handle(MarketOrderPaymentRequestedEvent event) {
+        cashFacade.handle(event);
     }
 }
