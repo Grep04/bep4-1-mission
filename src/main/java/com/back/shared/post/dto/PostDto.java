33beabea381555a5d8d5e1b838ac787a9371d2d3
NamSangwon <nsw3265@naver.com>
2025-12-29 10:24:44 +0900

    0031

--- src/main/java/com/back/shared/post/dto/PostDto.java
+++ src/main/java/com/back/shared/post/dto/PostDto.java
@@ -1,12 +1,19 @@
 package com.back.shared.post.dto;
 
 import com.back.boundedContext.post.domain.Post;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@AllArgsConstructor
+// Lombok이 생성자를 만들 때, 그 생성자 위에 특정한 어노테이션을 붙여달라고 지시
+// @JsonCreator: Jackson이 JSON 데이터를 자바 객체로 역직렬화할 때
+// 기본 생성자가 아닌 이 생성자를 사용해서 객체를 만들라고 지정
+// ※ Dto 객체의 필드에 final이 붙어 불변 객체로 만들 때 유용
+@AllArgsConstructor(
+    onConstructor_ = @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+)
 @Getter
 public class PostDto {
     private final int id;
