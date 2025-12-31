16ab7aa94bf1fe1303efcb8764e93f288a081f82
NamSangwon <nsw3265@naver.com>
2025-12-31 11:15:22 +0900

    0049

--- src/main/java/com/back/boundedContext/payout/in/PayoutDataInit.java
+++ src/main/java/com/back/boundedContext/payout/in/PayoutDataInit.java
@@ -30,18 +30,18 @@ public class PayoutDataInit {
     private final PayoutDataInit self;
     private final PayoutFacade payoutFacade;
     private final JobOperator jobOperator;
-    private final Job payoutCollectItemsJob;
+    private final Job payoutCollectItemsAndCompletePayoutsJob;
 
     public PayoutDataInit(
             @Lazy PayoutDataInit self,
             PayoutFacade payoutFacade,
             JobOperator jobOperator,
-            Job payoutCollectItemsJob
+            Job payoutCollectItemsAndCompletePayoutsJob
     ) {
         this.self = self;
         this.payoutFacade = payoutFacade;
         this.jobOperator = jobOperator;
-        this.payoutCollectItemsJob = payoutCollectItemsJob;
+        this.payoutCollectItemsAndCompletePayoutsJob = payoutCollectItemsAndCompletePayoutsJob;
     }
 
     @Bean
@@ -50,14 +50,13 @@ public class PayoutDataInit {
         return args -> {
             self.forceMakePayoutReadyCandidatesItems();
             self.collectPayoutItemsMore();
-            self.runCollectPayoutItemsBatchJob();
             self.completePayoutsMore();
+            self.runCollectItemsAndCompletePayoutsBatchJob();
         };
     }
 
     @Transactional
     public void forceMakePayoutReadyCandidatesItems() {
-        // reflection을 통해 테스팅을 위한 날짜 조작
         payoutFacade.findPayoutCandidateItems().forEach(item -> {
             Util.reflection.setField(
                     item,
@@ -69,11 +68,19 @@ public class PayoutDataInit {
 
     @Transactional
     public void collectPayoutItemsMore() {
-        payoutFacade.collectPayoutItemsMore(4);
+        payoutFacade.collectPayoutItemsMore(2);
+    }
+
+    @Transactional
+    public void completePayoutsMore() {
+        payoutFacade.completePayoutsMore(1);
     }
 
     // 배치 잡 실행
-    public void runCollectPayoutItemsBatchJob() {
+    // JobOperator를 사용하여 payoutCollectItemsAndCompletePayoutsJob 실행
+    // 현재 날짜를 job parameter로 전달
+    // collectPayoutItemsMore와 completePayoutsMore 메서드가 CHUNK_SIZE 만큼 데이터를 처리하도록 함
+    public void runCollectItemsAndCompletePayoutsBatchJob() {
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString(
                         "runDate",
@@ -82,7 +89,7 @@ public class PayoutDataInit {
                 .toJobParameters();
 
         try {
-            JobExecution execution = jobOperator.start(payoutCollectItemsJob, jobParameters);
+            JobExecution execution = jobOperator.start(payoutCollectItemsAndCompletePayoutsJob, jobParameters);
         } catch (JobInstanceAlreadyCompleteException e) {
             log.error("Job instance already complete", e);
         } catch (JobExecutionAlreadyRunningException e) {
@@ -92,12 +99,5 @@ public class PayoutDataInit {
         } catch (JobRestartException e) {
             log.error("job restart exception", e);
         }
-    }
-
-    @Transactional
-    public void completePayoutsMore() {
-        payoutFacade.completePayoutsMore(4);
-        payoutFacade.completePayoutsMore(2);
-        payoutFacade.completePayoutsMore(2);
     }
 }
